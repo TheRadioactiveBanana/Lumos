bda413b95adba8a6240d29c5227ed75c04fb9b14
TheRadioactiveBanana <89061718+TheRadioactiveBanana@users.noreply.github.com>
2023-11-01 02:49:15 +0530

    ANUKE FORGIVE ME HE FORGOT THE ; PLEASE DONT KILL US ALL

--- src/main/java/lumos/content/LumosBlocks.java
+++ src/main/java/lumos/content/LumosBlocks.java
@@ -4,6 +4,10 @@ import mindustry.world.Block;
 
 public class LumosBlocks {
 
-    public static Block coreSpall, coreSegment, coreCrown, graphiteBaker, aluminothermicFurnace, electricFusionChamber, biofuge, oilSolidifier, oxygenConcentrator, radonReactor, coguniteCompressor, isobaricOxidizer, cryogenicDistiller, thermiteSynthesizer, thermalOilCracker, catalystOilCracker, pitchblendeSifter, uraniniteRefinery, superfluidSynthesizer, bioreactor, acidReductor, energyCrystalizer, carbonTower, petrolTower, thermalTower, nuclearTower, lumaTower, lumaNode, ionLink, lumaLink, alloyBattery, foundationalBattery, trio, fracture, spark, cauldron, repel, decrept, impose, clasp, spasm, stasis, demise, manifest
+    public static Block coreSpall, coreSegment, coreCrown, graphiteBaker, aluminothermicFurnace, electricFusionChamber, biofuge, 
+        oilSolidifier, oxygenConcentrator, radonReactor, coguniteCompressor, isobaricOxidizer, cryogenicDistiller, thermiteSynthesizer, 
+        thermalOilCracker, catalystOilCracker, pitchblendeSifter, uraniniteRefinery, superfluidSynthesizer, bioreactor, acidReductor, energyCrystalizer,
+        carbonTower, petrolTower, thermalTower, nuclearTower, lumaTower, lumaNode, ionLink, lumaLink, alloyBattery, foundationalBattery, trio, fracture, 
+        spark, cauldron, repel, decrept, impose, clasp, spasm, stasis, demise, manifest;
 
 }
