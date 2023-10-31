0c77965dcc3e1cc11985949175403cb672f19d80
TheRadioactiveBanana <89061718+TheRadioactiveBanana@users.noreply.github.com>
2023-11-01 01:40:12 +0530

    Comit
    AWSFAWEGHDKYD

---
+++ src/main/java/lumos/LumosMod.java
@@ -0,0 +1,37 @@
+package lumos;
+
+import arc.ApplicationListener;
+import mindustry.mod.Mod;
+
+public class LumosMod extends Mod implements ApplicationListener {
+
+    public LumosMod(){
+
+    }
+
+    @Override
+    public void init(){
+
+    }
+
+    @Override
+    public void loadContent(){
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void exit() {
+
+    }
+
+}
