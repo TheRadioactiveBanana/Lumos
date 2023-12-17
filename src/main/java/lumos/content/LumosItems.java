11dde3f476bf8e14573b07a07bc47f2e7acc8010
Twcash <131608563+Twcash@users.noreply.github.com>
2023-12-17 16:41:38 -0600

    Update LumosItems.java

--- src/main/java/lumos/content/LumosItems.java
+++ src/main/java/lumos/content/LumosItems.java
@@ -5,5 +5,14 @@ import mindustry.type.Item;
 public class LumosItems {
 
     public static Item luminite, manganese, fullerine, uraninite, quartz, sulphur, uranium, thermite, sapphire, cogunite, lumosCrystal, plutonium, silicaGlass;
-
+    public static void load(){
+        luminite = new Item("luminite", Color.valueOf("7575c8")){{
+         hardness = 1;
+            cost = .40f;
+        }};
+        manganese = new Item("manganese", Color.valueOf("7575c8")){{
+         hardness = 2;
+            cost = .45f;
+        }};
+        
 }
