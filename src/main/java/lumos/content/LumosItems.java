f7fee40a1c2d1ec5bcddb1a67e94401c8626a477
TheRadioactiveBanana <89061718+TheRadioactiveBanana@users.noreply.github.com>
2023-12-19 04:00:49 +0530

    Update LumosItems.java

--- src/main/java/lumos/content/LumosItems.java
+++ src/main/java/lumos/content/LumosItems.java
@@ -6,6 +6,7 @@ import mindustry.type.Item;
 public class LumosItems {
 
     public static Item luminite, manganese, fullerine, uraninite, quartz, sulphur, uranium, thermite, sapphire, cogunite, lumosCrystal, plutonium, silicaGlass;
+    
     public static void load() {
         luminite = new Item("luminite", Color.valueOf("7575c8")) {{
             hardness = 1;
@@ -19,46 +20,45 @@ public class LumosItems {
             hardness = 2;
             cost = 0.45f;
         }};
-                uraninite = new Item("uraninite", Color.valueOf("afbdab")) {{
+        uraninite = new Item("uraninite", Color.valueOf("afbdab")) {{
             hardness = 2;
             cost = 0.45f;
         }};
-                quartz = new Item("quartz", Color.valueOf("afbdab")) {{
+        quartz = new Item("quartz", Color.valueOf("afbdab")) {{
             hardness = 2;
             cost = 0.45f;
         }};
-                sulphur = new Item("sulphur", Color.valueOf("afbdab")) {{
+        sulphur = new Item("sulphur", Color.valueOf("afbdab")) {{
             hardness = 2;
             cost = 0.45f;
         }};
-                uranium = new Item("uranium", Color.valueOf("afbdab")) {{
+        uranium = new Item("uranium", Color.valueOf("afbdab")) {{
             hardness = 2;
             cost = 0.45f;
         }};
-                thermite = new Item("thermite", Color.valueOf("afbdab")) {{
+        thermite = new Item("thermite", Color.valueOf("afbdab")) {{
             hardness = 2;
             cost = 0.45f;
         }};
-                sapphire = new Item("sapphire", Color.valueOf("afbdab")) {{
+        sapphire = new Item("sapphire", Color.valueOf("afbdab")) {{
             hardness = 2;
             cost = 0.45f;
         }};
-                cogunite = new Item("cogunite", Color.valueOf("afbdab")) {{
+        cogunite = new Item("cogunite", Color.valueOf("afbdab")) {{
             hardness = 2;
             cost = 0.45f;
         }};
-                lumosCrystal = new Item("lumosCrystal", Color.valueOf("afbdab")) {{
+        lumosCrystal = new Item("lumosCrystal", Color.valueOf("afbdab")) {{
             hardness = 2;
             cost = 0.45f;
         }};
-                plutonium = new Item("plutonium", Color.valueOf("afbdab")) {{
+        plutonium = new Item("plutonium", Color.valueOf("afbdab")) {{
             hardness = 2;
             cost = 0.45f;
         }};
-                silicaGlass = new Item("silicaGlass", Color.valueOf("afbdab")) {{
+        silicaGlass = new Item("silicaGlass", Color.valueOf("afbdab")) {{
             hardness = 2;
             cost = 0.45f;
         }};
     }
-        
 }
