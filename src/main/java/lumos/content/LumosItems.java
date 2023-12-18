61ea7c1859df34bf5a47be599274859f6119c21c
TheRadioactiveBanana <89061718+TheRadioactiveBanana@users.noreply.github.com>
2023-12-19 03:50:27 +0530

    the hell is wrong with you

--- src/main/java/lumos/content/LumosItems.java
+++ src/main/java/lumos/content/LumosItems.java
@@ -1,18 +1,19 @@
 package lumos.content;
 
+import arc.graphics.Color;
 import mindustry.type.Item;
 
 public class LumosItems {
 
     public static Item luminite, manganese, fullerine, uraninite, quartz, sulphur, uranium, thermite, sapphire, cogunite, lumosCrystal, plutonium, silicaGlass;
-    public static void load(){
-        luminite = new Item("luminite", Color.valueOf("7575c8")){{
-         hardness = 1;
-            cost = .40f;
+    public static void load() {
+        luminite = new Item("luminite", Color.valueOf("7575c8")) {{
+            hardness = 1;
+            cost = 0.40f;
         }};
-        manganese = new Item("manganese", Color.valueOf("7575c8")){{
-         hardness = 2;
-            cost = .45f;
+        manganese = new Item("manganese", Color.valueOf("7575c8")) {{
+            hardness = 2;
+            cost = 0.45f;
         }};
     }
         
